47f98ac690be1d644163e55af44c88590f9ac75c
Rob Irizarry <robzarry@gmail.com>
2012-02-23 21:04:14 -0700

    Skeleton all with gemfile updated for Twitter Bootstrap, Formtastic & PDFTK

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Formsapp::Application.config.secret_token = 'dc9652fad9871ca71a7eb3282d601f4aa7393e016c265714f30508e11ac789d2d7a01377247bc93405ce9d485970dca0bf32c8ec8152af95cfa6855f94d2ba6c'
