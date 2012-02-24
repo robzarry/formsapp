4e4a9166849cb13ea60b5c380c3b245003afbcc3
Ryan Stout <ryanstout@gmail.com>
2012-02-24 15:55:26 -0700

    More sample code for messing with pdfs

--- lib/test_pdf.rb
+++ lib/test_pdf.rb
@@ -1,2 +1,14 @@
 file_name = "public/pdfs/f1023.pdf"
 pdf = Pdftk::PDF.new(file_name)
+
+# list all the fields
+pdf.fields
+
+# See the first field
+pdf.fields[0]
+
+# Get the value of the first field
+pdf.fields[0].value
+
+# See all of the values
+pdf.fields.map(&:values)
